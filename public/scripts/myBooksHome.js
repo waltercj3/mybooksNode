74061f79f4ff4d4ea697d621960a8a3534e91f11
johnwalt8 <46585029+johnwalt8@users.noreply.github.com>
2019-02-25 20:36:03 -0800

    added picture to home, css for home from myBooksHome.js

--- public/scripts/myBooksHome.js
+++ public/scripts/myBooksHome.js
@@ -2,3 +2,18 @@
 
 'use strict';
 
+const MBHG = {
+    doc: document,
+    makeStyle: null
+};
+
+MBHG.makeStyle = function () {
+    var sheet, rule, index;
+    sheet = MBHG.doc.head.appendChild(MBHG.doc.createElement('style')).sheet;
+    index = sheet.cssRules.length;
+    rule = "div img {display:block; margin: 21px auto;}";
+    sheet.insertRule(rule, index);
+};
+
+//MBHG.makeStyle();
+document.addEventListener('DOMContentLoaded', MBHG.makeStyle);
